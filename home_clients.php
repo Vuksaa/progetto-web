7b2e73abc11f5b2b40286d8f5d6a82c83f2452b7
Vuksaa <mihajlovic.vuksa@gmail.com>
2019-05-01 14:10:17 +0200

    Separate container and accordion divs instead of having one div with both classes

--- home_clients.php
+++ home_clients.php
@@ -73,136 +73,139 @@
     </div>
   </nav>
 
-  <div class="container accordion mt-4 mb-4" id="mainAccordion">
-    <div class="card main-card">
-      <h1 class="mb-0">
-        <button class="btn btn-secondary btn-lg btn-block active" data-toggle="collapse" data-target="#collapseFavouriteRestaurants" aria-expanded="true" aria-controls="collapseFavouriteRestaurants">
-          Your Favourite Restaurants
-        </button>
-      </h1>
-      <div id="collapseFavouriteRestaurants" class="collapse show" aria-labelledby="headingFavouriteRestaurants" data-parent="#mainAccordion">
-        <div class="card-body">
-          <form class="form-inline card-searchbar">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-          <div class="row">
-            <?php
-              if ($favProviders = $conn->query("SELECT p.provider_id, p.provider_name, t.type_name
-                                          FROM client_provider cp
-                                          JOIN provider p
-                                          ON cp.provider_id = p.provider_id
-                                          LEFT JOIN type t
-                                          ON p.type_id = t.type_id
-                                          WHERE cp.client_id = '".$_SESSION['user_id']."'")) {
-              while ($providerRow = $favProviders->fetch_assoc()) {
-            ?>
-            <div class="card col-sm-3">
-              <div class="card-body">
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="client_provider_remove_form">
-                  <?php echo "<input type='number' name='providerId' value='".$providerRow['provider_id']."' hidden>" ; ?>
-                  <h5 class="card-title"><?php echo $providerRow['provider_name'] ?>
-                    <button type="submit" name="btnClientProviderRemove" class="btn btn-link" value="clientProviderRemove"><i class="fas fa-star"></i></button>
-                  </h5>
-                </form>
-                <h6 class="card-subtitle mb-2 text-muted"><?php echo $providerRow['type_name'] ?></h6>
-                <p class="card-text">
-                  <?php
-                    if ($providerCategories = $conn->query("SELECT c.category_name
-                                          FROM provider p
-                                          LEFT JOIN provider_category pc
-                                          ON p.provider_id = pc.provider_id
-                                          LEFT JOIN category c
-                                          ON c.category_id = pc.category_id
-                                          WHERE p.provider_id = '".$providerRow['provider_id']."'")) {
-                      while ($categoryRow = $providerCategories->fetch_assoc()) {
-                        echo "<span class='badge badge-pill badge-info'>".$categoryRow['category_name']."</span>";
+  <div class="container mt-4 mb-4">
+    <h3 class="pb-2">Provider list</h3>
+    <div class="container accordion mt-4 mb-4" id="mainAccordion">
+      <div class="card main-card">
+        <h1 class="mb-0">
+          <button class="btn btn-secondary btn-lg btn-block active" data-toggle="collapse" data-target="#collapseFavouriteRestaurants" aria-expanded="true" aria-controls="collapseFavouriteRestaurants">
+            Your Favourite Restaurants
+          </button>
+        </h1>
+        <div id="collapseFavouriteRestaurants" class="collapse show" aria-labelledby="headingFavouriteRestaurants" data-parent="#mainAccordion">
+          <div class="card-body">
+            <form class="form-inline card-searchbar">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <div class="row">
+              <?php
+                if ($favProviders = $conn->query("SELECT p.provider_id, p.provider_name, t.type_name
+                                            FROM client_provider cp
+                                            JOIN provider p
+                                            ON cp.provider_id = p.provider_id
+                                            LEFT JOIN type t
+                                            ON p.type_id = t.type_id
+                                            WHERE cp.client_id = '".$_SESSION['user_id']."'")) {
+                while ($providerRow = $favProviders->fetch_assoc()) {
+              ?>
+              <div class="card col-sm-3">
+                <div class="card-body">
+                  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="client_provider_remove_form">
+                    <?php echo "<input type='number' name='providerId' value='".$providerRow['provider_id']."' hidden>" ; ?>
+                    <h5 class="card-title"><?php echo $providerRow['provider_name'] ?>
+                      <button type="submit" name="btnClientProviderRemove" class="btn btn-link" value="clientProviderRemove"><i class="fas fa-star"></i></button>
+                    </h5>
+                  </form>
+                  <h6 class="card-subtitle mb-2 text-muted"><?php echo $providerRow['type_name'] ?></h6>
+                  <p class="card-text">
+                    <?php
+                      if ($providerCategories = $conn->query("SELECT c.category_name
+                                            FROM provider p
+                                            LEFT JOIN provider_category pc
+                                            ON p.provider_id = pc.provider_id
+                                            LEFT JOIN category c
+                                            ON c.category_id = pc.category_id
+                                            WHERE p.provider_id = '".$providerRow['provider_id']."'")) {
+                        while ($categoryRow = $providerCategories->fetch_assoc()) {
+                          echo "<span class='badge badge-pill badge-info'>".$categoryRow['category_name']."</span>";
+                        }
+                        $providerCategories->close();
                       }
-                      $providerCategories->close();
-                    }
-                  ?>
-                </p>
-                <div class="btn-group btn-group-justified">
-                  <button class="btn btn-primary inline" data-toggle="modal" data-target="#peekOnProvider">Peek</button>
-                  <button class="btn btn-primary inline btn-place-order" data-provider-id="<?php echo $providerRow['provider_id']; ?>">Order</button>
+                    ?>
+                  </p>
+                  <div class="btn-group btn-group-justified">
+                    <button class="btn btn-primary inline" data-toggle="modal" data-target="#peekOnProvider">Peek</button>
+                    <button class="btn btn-primary inline btn-place-order" data-provider-id="<?php echo $providerRow['provider_id']; ?>">Order</button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <?php
+              <?php
+                  }
+                  $favProviders->close();
+                } else {
+                  echo "Query failed";
                 }
-                $favProviders->close();
-              } else {
-                echo "Query failed";
-              }
-             ?>
+               ?>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="card main-card">
-      <h1 class="mb-0">
-        <button class="btn btn-secondary btn-lg btn-block active" data-toggle="collapse" data-target="#collapseAllRestaurants" aria-expanded="false" aria-controls="collapseAllRestaurants">
-          Restaurants
-        </button>
-      </h1>
-      <div id="collapseAllRestaurants" class="collapse" aria-labelledby="headingRestaurants" data-parent="#mainAccordion">
-        <div class="card-body">
-          <form class="form-inline card-searchbar">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-          <div class="row">
-            <?php
-              // TODO: use one query and separate favourite and non-favourite restaurants in php?
-              if ($allProviders = $conn->query("SELECT p.provider_id, p.provider_name, t.type_name
-                                          FROM provider p
-                                          LEFT JOIN type t
-                                          ON p.type_id = t.type_id
-                                          WHERE p.provider_id NOT IN (SELECT p.provider_id
-                                                                      FROM client_provider cp
-                                                                      JOIN provider p
-                                                                      ON cp.provider_id = p.provider_id
-                                                                      WHERE cp.client_id = '".$_SESSION['user_id']."')")) {
-              while ($providerRow = $allProviders->fetch_assoc()) {
-            ?>
-            <div class="card col-sm-3">
-              <div class="card-body">
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="client_provider_add_form">
-                  <?php echo "<input type='number' name='providerId' value='".$providerRow['provider_id']."' hidden>" ; ?>
-                  <h5 class="card-title"><?php echo $providerRow['provider_name'] ?>
-                    <button type="submit" name="btnClientProviderAdd" class="btn btn-link" value="clientProviderAdd"><i class="far fa-star"></i></button>
-                  </h5>
-                </form>
-                <h6 class="card-subtitle mb-2 text-muted"><?php echo $providerRow['type_name'] ?></h6>
-                <p class="card-text">
-                  <?php
-                    if ($providerCategories = $conn->query("SELECT c.category_name
-                                          FROM provider p
-                                          LEFT JOIN provider_category pc
-                                          ON p.provider_id = pc.provider_id
-                                          LEFT JOIN category c
-                                          ON c.category_id = pc.category_id
-                                          WHERE p.provider_id = '".$providerRow['provider_id']."'")) {
-                      while ($categoryRow = $providerCategories->fetch_assoc()) {
-                        echo "<span class='badge badge-pill badge-info'>".$categoryRow['category_name']."</span>";
+      <div class="card main-card">
+        <h1 class="mb-0">
+          <button class="btn btn-secondary btn-lg btn-block active" data-toggle="collapse" data-target="#collapseAllRestaurants" aria-expanded="false" aria-controls="collapseAllRestaurants">
+            Restaurants
+          </button>
+        </h1>
+        <div id="collapseAllRestaurants" class="collapse" aria-labelledby="headingRestaurants" data-parent="#mainAccordion">
+          <div class="card-body">
+            <form class="form-inline card-searchbar">
+              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+            <div class="row">
+              <?php
+                // TODO: use one query and separate favourite and non-favourite restaurants in php?
+                if ($allProviders = $conn->query("SELECT p.provider_id, p.provider_name, t.type_name
+                                            FROM provider p
+                                            LEFT JOIN type t
+                                            ON p.type_id = t.type_id
+                                            WHERE p.provider_id NOT IN (SELECT p.provider_id
+                                                                        FROM client_provider cp
+                                                                        JOIN provider p
+                                                                        ON cp.provider_id = p.provider_id
+                                                                        WHERE cp.client_id = '".$_SESSION['user_id']."')")) {
+                while ($providerRow = $allProviders->fetch_assoc()) {
+              ?>
+              <div class="card col-sm-3">
+                <div class="card-body">
+                  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="client_provider_add_form">
+                    <?php echo "<input type='number' name='providerId' value='".$providerRow['provider_id']."' hidden>" ; ?>
+                    <h5 class="card-title"><?php echo $providerRow['provider_name'] ?>
+                      <button type="submit" name="btnClientProviderAdd" class="btn btn-link" value="clientProviderAdd"><i class="far fa-star"></i></button>
+                    </h5>
+                  </form>
+                  <h6 class="card-subtitle mb-2 text-muted"><?php echo $providerRow['type_name'] ?></h6>
+                  <p class="card-text">
+                    <?php
+                      if ($providerCategories = $conn->query("SELECT c.category_name
+                                            FROM provider p
+                                            LEFT JOIN provider_category pc
+                                            ON p.provider_id = pc.provider_id
+                                            LEFT JOIN category c
+                                            ON c.category_id = pc.category_id
+                                            WHERE p.provider_id = '".$providerRow['provider_id']."'")) {
+                        while ($categoryRow = $providerCategories->fetch_assoc()) {
+                          echo "<span class='badge badge-pill badge-info'>".$categoryRow['category_name']."</span>";
+                        }
+                        $providerCategories->close();
                       }
-                      $providerCategories->close();
-                    }
-                  ?>
-                </p>
-                <div class="btn-group btn-group-justified">
-                  <button class="btn btn-primary inline" data-toggle="modal" data-target="#peekOnProvider">Peek</button>
-                  <button class="btn btn-primary inline btn-place-order" data-provider-id="<?php echo $providerRow['provider_id']; ?>">Order</button>
+                    ?>
+                  </p>
+                  <div class="btn-group btn-group-justified">
+                    <button class="btn btn-primary inline" data-toggle="modal" data-target="#peekOnProvider">Peek</button>
+                    <button class="btn btn-primary inline btn-place-order" data-provider-id="<?php echo $providerRow['provider_id']; ?>">Order</button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <?php
+              <?php
+                  }
+                  $allProviders->close();
+                } else {
+                  echo "Query failed";
                 }
-                $allProviders->close();
-              } else {
-                echo "Query failed";
-              }
-             ?>
+               ?>
+            </div>
           </div>
         </div>
       </div>
