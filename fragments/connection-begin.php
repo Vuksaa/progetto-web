878316147e7443989341b2c3bd4e74e5fe6c8f35
Vuksaa <mihajlovic.vuksa@gmail.com>
2019-05-19 10:54:20 +0200

    Change variables' names in connection-begin fragment to avoid overwriting variables

--- fragments/connection-begin.php
+++ fragments/connection-begin.php
@@ -1,9 +1,9 @@
 <?php
-  $servername = "localhost";
-  $username = "root";
-  $password="";
-  $db = "uni_web_prod";
-  $conn = new mysqli($servername, $username,$password,$db);
+  $db_login_servername = "localhost";
+  $db_login_username = "root";
+  $db_login_password="";
+  $db_login_dbname = "uni_web_prod";
+  $conn = new mysqli($db_login_servername, $db_login_username, $db_login_password, $db_login_dbname);
   if($conn->connect_error) {
     die("Connection failed: ".$conn->connect_error);
   }
