271d011450a7b0339588c79f8792eba331f41a55
Vuksaa <mihajlovic.vuksa@gmail.com>
2019-06-18 17:06:50 +0200

    In home_providers, periodically fetch new orders and add them to the "Incoming Orders" section

--- home_providers.php
+++ home_providers.php
@@ -72,174 +72,254 @@ if (!isset($_SESSION['statement_fetch_awaiting_orders'])) {
   );
 }
 ?>
+var lastOrderFetch = 0;
 function fetchAwaitingOrders() {
-  $.ajax({
+  $.post({
     url: "ajax/fetch_awaiting_orders.php",
-    method: get,
-    success: function(response) {
-
-    },
-    complete: function() {
-      setTimeout(fetchAwaitingOrders, 5000);
+    data: {
+      // php's mktime() function creates a 10-digit unix timestamp while JS creates a 13-digit timestamp, so we have to shorten the JS timestamp
+      timestamp: lastOrderFetch.toString().substring(0, 10)
     }
   })
+  .done(function(response) {
+    if (response !== 'EMPTY') {
+      var responseArray = JSON.parse(response)
+      // take all elements with a distinct order_id and create an "order" element for each order
+      // distinct filter taken from https://stackoverflow.com/a/47313334
+      var orders = []
+      $.each(responseArray.filter(
+        (arr, index, self) => index === self.findIndex(
+          (t) => (t.order_id === arr.order_id))
+        ),
+        function(index, it) {
+          orders.push({
+            order_id: it.order_id,
+            client_name: it.client_name,
+            creation_timestamp: it.creation_timestamp,
+            order_address: it.order_address,
+            status_id: it.status_id,
+            products: []
+          })
+        }
+      )
+      // fill the orders with their respective products
+      $.each(responseArray, function(index, it) {
+        orders.filter(self => self.order_id === it.order_id)[0].products.push({
+        // orders[it.order_id].products.push({
+          product_name: it.product_name,
+          quantity: it.quantity,
+          notes: it.notes
+        })
+      })
+      // order by date
+      orders.sort(function(a, b) { return Date.parse(b.creation_timestamp) - Date.parse(a.creation_timestamp) })
+      $.each(orders, function(index, it) {
+        var element = `
+        <div class="card orderCard mb-3" data-orderId="` + it.order_id + `">
+          <div class="card-body">
+            <div class="card-title">
+              <h7 class="text-muted float-right">` + it.creation_timestamp + `</h7>
+              <h5>` + (it.status_id != 2 ? it.client_name + `'s order` : `<strike>` + it.client_name + `'s order</strike>`) + `</h5>
+            </div>
+            <h5 class="card-title"></h5>
+            <h6 class="card-subtitle mb-2 text-muted">` + it.order_address + `</h6>`
+        $.each(it.products, function(index, orderedProduct) {
+          element += `
+          <div class="card-text">
+            <div class="border-top border-bottom p-2">
+              <div class="row">
+                <label class="col-sm-2 col-form-label border-right">Product</label>
+                <label class="col col-form-label">` + orderedProduct.product_name + `</label>
+              </div>
+              <div class="row">
+                <label class="col-sm-2 col-form-label border-right">Quantity</label>
+                <label class="col col-form-label">` + orderedProduct.quantity + `</label>
+              </div>`
+          if (orderedProduct.notes != "") {
+            element += `
+            <div class="row">
+              <label class="col-sm-2 col-form-label border-right">Notes</label>
+              <label class="col col-form-label">` + orderedProduct.notes + `</label>
+            </div>`
+          }
+          element += `
+              </div>
+            </div>`
+        })
+        element += `
+            <div class="btn-group btn-group-justified pt-2">
+              <button class="btn btn-primary inline btnAccept" onclick="btnAcceptClick(this)">Accept</button>
+              <button class="btn btn-primary inline btnReject" onclick="btnRejectClick(this)">Reject</button>
+            </div>
+          </div>
+        </div>`
+        $("#ordersIncoming").append(element)
+      })
+    }
+  })
+  .always(function() {
+    setTimeout(fetchAwaitingOrders, 1000)
+    lastOrderFetch = $.now()
+  })
 }
+
 $(function() {
   /* Set navbar voice active with respective screen reader functionality */
   var element = $("nav div ul li a:contains('Home')");
   var parent = element.parent();
   element.append( "<span class='sr-only'>(current)</span>" );
   parent.addClass("active");
-
   // fetch all orders and place them in their section.
   $.post({
     url: "ajax/fetch_recent_orders.php",
     data: {
       order_recency: "ALL"
-    },
-    success: function(response) {
-      if (response === 'ERROR') {
-        alert("ERROR");
-      } else {
-        var responseArray = JSON.parse(response)
-        // take all elements with a distinct order_id and create an "order" element for each order
-        // distinct filter taken from https://stackoverflow.com/a/47313334
-        var allOrders = []
-        $.each(responseArray.filter(
-          (arr, index, self) => index === self.findIndex(
-            (t) => (t.order_id === arr.order_id))
-          ),
-          function(index, it) {
-            allOrders.push({
-              order_id: it.order_id,
-              client_name: it.client_name,
-              creation_timestamp: it.creation_timestamp,
-              order_address: it.order_address,
-              status_id: it.status_id,
-              products: []
-            })
-          }
-        )
-        // fill the orders with their respective products
-        $.each(responseArray, function(index, it) {
-          allOrders.filter(self => self.order_id === it.order_id)[0].products.push({
-          // allOrders[it.order_id].products.push({
-            product_name: it.product_name,
-            quantity: it.quantity,
-            notes: it.notes
-          })
-        })
-        // order by date
-        allOrders.sort(function(a, b) { return Date.parse(b.creation_timestamp) - Date.parse(a.creation_timestamp) })
-        // put each order in its section
-        $.each(allOrders, function(index, it) {
-          var element = `
-          <div class="card orderCard mb-3" data-orderId="` + it.order_id + `">
-            <div class="card-body">
-              <div class="card-title">
-                <h7 class="text-muted float-right">` + it.creation_timestamp + `</h7>
-                <h5>` + (it.status_id != 2 ? it.client_name + `'s order` : `<strike>` + it.client_name + `'s order</strike>`) + `</h5>
-              </div>
-              <h5 class="card-title"></h5>
-              <h6 class="card-subtitle mb-2 text-muted">` + it.order_address + `</h6>`
-          if (it.status_id == 4) {
-            $.each(it.products, function(index, orderedProduct) {
-              element += `
-              <div class="card-text">
-                <div class="border-top border-bottom p-2">
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label border-right">Product</label>
-                    <label class="col col-form-label">` + orderedProduct.product_name + `</label>
-                  </div>
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label border-right">Quantity</label>
-                    <label class="col col-form-label">` + orderedProduct.quantity + `</label>
-                  </div>`
-              if (orderedProduct.notes != "") {
-                element += `
-                <div class="row">
-                  <label class="col-sm-2 col-form-label border-right">Notes</label>
-                  <label class="col col-form-label">` + orderedProduct.notes + `</label>
-                </div>`
-              }
-              element += `
-                  </div>
-                </div>`
-            })
-            element += `
-                <div class="btn-group btn-group-justified pt-2">
-                  <button class="btn btn-primary inline btnAccept" onclick="btnAcceptClick(this)">Accept</button>
-                  <button class="btn btn-primary inline btnReject" onclick="btnRejectClick(this)">Reject</button>
-                </div>
-              </div>
-            </div>`
-            $("#ordersIncoming").prepend(element)
-          } else if (it.status_id == 1) {
-            $.each(it.products, function(index, orderedProduct) {
-              element += `
-              <div class="card-text">
-                <div class="border-top border-bottom p-2">
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label border-right">Product</label>
-                    <label class="col col-form-label">` + orderedProduct.product_name + `</label>
-                  </div>
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label border-right">Quantity</label>
-                    <label class="col col-form-label">` + orderedProduct.quantity + `</label>
-                  </div>`
-              if (orderedProduct.notes != "") {
-                element += `
-                <div class="row">
-                  <label class="col-sm-2 col-form-label border-right">Notes</label>
-                  <label class="col col-form-label">` + orderedProduct.notes + `</label>
-                </div>`
-              }
-              element += `
-                  </div>
-                </div>`
-            })
-            element += `
-                <div class="btn-group btn-group-justified pt-2">
-                  <button class="btn btn-primary inline" onclick="btnCompleteClick(this)">Complete</button>
-                </div>
-              </div>
-            </div>`
-            $("#ordersAccepted").prepend(element)
-          } else {
-            $.each(it.products, function(index, orderedProduct) {
-              element += `
-              <div class="card-text">
-                <div class="border-top border-bottom p-2">
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label border-right">Product</label>
-                    <label class="col col-form-label">` + orderedProduct.product_name + `</label>
-                  </div>
-                  <div class="row">
-                    <label class="col-sm-2 col-form-label border-right">Quantity</label>
-                    <label class="col col-form-label">` + orderedProduct.quantity + `</label>
-                  </div>`
-              if (orderedProduct.notes != "") {
-                element += `
-                <div class="row">
-                  <label class="col-sm-2 col-form-label border-right">Notes</label>
-                  <label class="col col-form-label">` + orderedProduct.notes + `</label>
-                </div>`
-              }
-              element += `
-                  </div>
-                </div>`
-            })
-            element += `
-              </div>
-            </div>`
-            $("#ordersCompleted").prepend(element)
-          }
-        })
-      }
     }
   })
-
-  // fetchAwaitingOrders()
+  .done(function(response) {
+    if (response === 'ERROR') {
+      alert("ERROR");
+    } else {
+      var responseArray = JSON.parse(response)
+      // take all elements with a distinct order_id and create an "order" element for each order
+      // distinct filter taken from https://stackoverflow.com/a/47313334
+      var allOrders = []
+      $.each(responseArray.filter(
+        (arr, index, self) => index === self.findIndex(
+          (t) => (t.order_id === arr.order_id))
+        ),
+        function(index, it) {
+          allOrders.push({
+            order_id: it.order_id,
+            client_name: it.client_name,
+            creation_timestamp: it.creation_timestamp,
+            order_address: it.order_address,
+            status_id: it.status_id,
+            products: []
+          })
+        }
+      )
+      // fill the orders with their respective products
+      $.each(responseArray, function(index, it) {
+        allOrders.filter(self => self.order_id === it.order_id)[0].products.push({
+        // allOrders[it.order_id].products.push({
+          product_name: it.product_name,
+          quantity: it.quantity,
+          notes: it.notes
+        })
+      })
+      // order by date
+      allOrders.sort(function(a, b) { return Date.parse(b.creation_timestamp) - Date.parse(a.creation_timestamp) })
+      // put each order in its section
+      $.each(allOrders, function(index, it) {
+        var element = `
+        <div class="card orderCard mb-3" data-orderId="` + it.order_id + `">
+          <div class="card-body">
+            <div class="card-title">
+              <h7 class="text-muted float-right">` + it.creation_timestamp + `</h7>
+              <h5>` + (it.status_id != 2 ? it.client_name + `'s order` : `<strike>` + it.client_name + `'s order</strike>`) + `</h5>
+            </div>
+            <h5 class="card-title"></h5>
+            <h6 class="card-subtitle mb-2 text-muted">` + it.order_address + `</h6>`
+        if (it.status_id == 4) {
+          $.each(it.products, function(index, orderedProduct) {
+            element += `
+            <div class="card-text">
+              <div class="border-top border-bottom p-2">
+                <div class="row">
+                  <label class="col-sm-2 col-form-label border-right">Product</label>
+                  <label class="col col-form-label">` + orderedProduct.product_name + `</label>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label border-right">Quantity</label>
+                  <label class="col col-form-label">` + orderedProduct.quantity + `</label>
+                </div>`
+            if (orderedProduct.notes != "") {
+              element += `
+              <div class="row">
+                <label class="col-sm-2 col-form-label border-right">Notes</label>
+                <label class="col col-form-label">` + orderedProduct.notes + `</label>
+              </div>`
+            }
+            element += `
+                </div>
+              </div>`
+          })
+          element += `
+              <div class="btn-group btn-group-justified pt-2">
+                <button class="btn btn-primary inline btnAccept" onclick="btnAcceptClick(this)">Accept</button>
+                <button class="btn btn-primary inline btnReject" onclick="btnRejectClick(this)">Reject</button>
+              </div>
+            </div>
+          </div>`
+          $("#ordersIncoming").prepend(element)
+        } else if (it.status_id == 1) {
+          $.each(it.products, function(index, orderedProduct) {
+            element += `
+            <div class="card-text">
+              <div class="border-top border-bottom p-2">
+                <div class="row">
+                  <label class="col-sm-2 col-form-label border-right">Product</label>
+                  <label class="col col-form-label">` + orderedProduct.product_name + `</label>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label border-right">Quantity</label>
+                  <label class="col col-form-label">` + orderedProduct.quantity + `</label>
+                </div>`
+            if (orderedProduct.notes != "") {
+              element += `
+              <div class="row">
+                <label class="col-sm-2 col-form-label border-right">Notes</label>
+                <label class="col col-form-label">` + orderedProduct.notes + `</label>
+              </div>`
+            }
+            element += `
+                </div>
+              </div>`
+          })
+          element += `
+              <div class="btn-group btn-group-justified pt-2">
+                <button class="btn btn-primary inline" onclick="btnCompleteClick(this)">Complete</button>
+              </div>
+            </div>
+          </div>`
+          $("#ordersAccepted").prepend(element)
+        } else {
+          $.each(it.products, function(index, orderedProduct) {
+            element += `
+            <div class="card-text">
+              <div class="border-top border-bottom p-2">
+                <div class="row">
+                  <label class="col-sm-2 col-form-label border-right">Product</label>
+                  <label class="col col-form-label">` + orderedProduct.product_name + `</label>
+                </div>
+                <div class="row">
+                  <label class="col-sm-2 col-form-label border-right">Quantity</label>
+                  <label class="col col-form-label">` + orderedProduct.quantity + `</label>
+                </div>`
+            if (orderedProduct.notes != "") {
+              element += `
+              <div class="row">
+                <label class="col-sm-2 col-form-label border-right">Notes</label>
+                <label class="col col-form-label">` + orderedProduct.notes + `</label>
+              </div>`
+            }
+            element += `
+                </div>
+              </div>`
+          })
+          element += `
+            </div>
+          </div>`
+          $("#ordersCompleted").prepend(element)
+        }
+      })
+    }
+  })
+  .always(function() {
+      setTimeout(fetchAwaitingOrders, 1000)
+      lastOrderFetch = $.now()
+    })
 })
 
 function btnAcceptClick(e) {
