c247750e3fa91c9b9c85fd53122e92a54fac055a
Lorenzo Quadrelli <lorenzo.quadrelli96@gmail.com>
2019-06-28 16:33:46 +0200

    Translate products_providers to italian

--- products_providers.php
+++ products_providers.php
@@ -57,7 +57,7 @@
     <div class="card main-card">
       <h1 class="mb-0 form-group form-inline">
         <button class="btn btn-secondary btn-lg btn-block active" data-toggle="collapse" data-target="#collapseProduct" aria-expanded="true" aria-controls="collapseProducts">
-          Your products
+          I tuoi prodotti
         </button>
       </h1>
       <div id="collapseProduct" class="collapse show" aria-labelledby="headingFavouriteRestaurants">
@@ -66,7 +66,7 @@
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
-          <button type="button" class="btn btn-primary" value="productAdd" data-toggle="modal" data-target="#productAddModal">Add Product</button>
+          <button type="button" class="btn btn-primary" value="productAdd" data-toggle="modal" data-target="#productAddModal">Aggiungi prodotto</button>
           <div class="row">
             <?php
             if ($products = $conn->query("SELECT product_id, product_name, product_price, product_description
@@ -84,9 +84,9 @@
                     <button type="button" class="btn btn-primary inline" name="btnProductModify" value="productModify" data-toggle="modal" data-target="#productAddModal" data-id="<?php echo $productRow['product_id']?>"
                       data-name="<?php echo $productRow['product_name']?>" data-description="<?php echo $productRow['product_description']?>"
                        data-price="<?php echo $productRow['product_price']?>">
-                      Modify
+                      Modifica
                     </button>
-                    <button type="submit" name="btnProductRemove" class="btn btn-primary" value="productRemove">Remove</button>
+                    <button type="submit" name="btnProductRemove" class="btn btn-primary" value="productRemove">Rimuovi</button>
                   </div>
                 </form>
               </div>
@@ -108,7 +108,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="productAddModalLabel">Add a product</h5>
+          <h5 class="modal-title" id="productAddModalLabel">Aggiungi un prodotto</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
@@ -117,21 +117,21 @@
           <div class="modal-body">
             <input type="number" name="productId" value="" id="productId" hidden>
             <div class="form-group">
-              <label for="productName" class="col-form-label">Name:</label>
+              <label for="productName" class="col-form-label">Nome:</label>
               <input type="text" class="form-control" name="productName" id="productName" required>
             </div>
             <div class="form-group">
-              <label for="message-text" class="col-form-label">Description:</label>
+              <label for="message-text" class="col-form-label">Descrizione:</label>
               <textarea class="form-control" name="productDescription" id="productDescription"></textarea>
             </div>
             <div class="form-group">
-              <label for="productPrice" class="col-form-label">Price:</label>
+              <label for="productPrice" class="col-form-label">Prezzo:</label>
               <input type="number" step="any" class="form-control" name="productPrice" id="productPrice" required></input>
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="submit" name="btnProductAdd" value="productAdd" class="btn btn-primary" id="btnProductAdd">Add product</button>
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Chiudi</button>
+            <button type="submit" name="btnProductAdd" value="productAdd" class="btn btn-primary" id="btnProductAdd">Aggiungi prodotto</button>
           </div>
         </form>
       </div>
@@ -153,16 +153,16 @@
     var modal = $(this);
     modal.find('#productId').val(productId);
     if(button.val()=="productModify"){
-      modal.find('.modal-title').text("Modify the product");
+      modal.find('.modal-title').text("Modifica il prodotto");
       modal.find('#productName').val(productName);
       modal.find('#productDescription').val(productDescription);
       modal.find('#productPrice').val(productPrice);
-      modal.find('#btnProductAdd').text("Modify");
+      modal.find('#btnProductAdd').text("Modifica");
       modal.find('#btnProductAdd').val("productModify");
     } else {
-      modal.find('.modal-title').text("Add a product");
+      modal.find('.modal-title').text("Aggiungi un prodotto");
     }
-  })
+  });
   /* Set navbar voice active with respective screen reader functionality */
   var element = $("#navbarProducts");
   var parent = element.parent();
